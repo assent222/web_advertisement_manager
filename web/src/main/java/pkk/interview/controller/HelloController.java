cdc69cbee3425c433bf987f326700675c3588ecd
pkk <assent222@gmail.com>
2017-01-19 16:47:23 +0200

    init

--- web/src/main/java/pkk/interview/controller/HelloController.java
+++ web/src/main/java/pkk/interview/controller/HelloController.java
@@ -1,17 +1,22 @@
 package pkk.interview.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by root on 18.01.2017.
  */
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/hello/")
-    public String hello(){
-        return "index";
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String getMovie(@PathVariable String name, ModelMap model) {
+        model.addAttribute("name", name);
+        return "helloThymeleaf";
     }
 }
