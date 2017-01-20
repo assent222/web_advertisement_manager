c00daddfb609e76abf4d513a2480cd2708332c8e
pkk <assent222@gmail.com>
2017-01-21 00:01:26 +0200

    upgrade Spring Security to version 4+ add csrf support add ajax/json support

--- web/src/main/java/pkk/interview/controller/AjaxController.java
+++ web/src/main/java/pkk/interview/controller/AjaxController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pkk.interview.controller.message.UserJson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by root on 20.01.2017.
  */
@@ -15,9 +18,28 @@ import pkk.interview.controller.message.UserJson;
 @RequestMapping(path = "/ajax")
 public class AjaxController {
 
-    @RequestMapping(path = "/echo", method = RequestMethod.GET)
+    @RequestMapping("/get_json")
+    @ResponseBody
+    public Map getJson() {
+        Map map = new HashMap();
+        map.put("hello", "world");
+        return map;
+    }
+
+    @RequestMapping("/echo")
     @ResponseBody
     public UserJson echo(@RequestBody UserJson obj) {
         return obj;
     }
+
+    /*
+    $.ajax({
+        type: 'post',
+                url: 'http://localhost:8080/test/ajax/echo',
+                data: JSON.stringify({"a":"123"}),
+        contentType: "application/json; charset=utf-8",
+    }).done(function (data) {
+        console.dir(data);
+    });
+    */
 }
